30d59d2ac4d6ec8139b4a32c06e83e9fcda9df18
Alan912-dev <alanlushchichenko@gmail.com>
2026-09-03 22:16:55 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		String Alan = ("Happy Birthday to you.");
+		String Lushchichenko = ("Happy Birthday dear");
+		String Nina = ("Lev.");
+		System.out.println(Alan);
+		System.out.println(Alan);
+		System.out.println(Lushchichenko + Nina);
+		System.out.println(Alan);
 	}
 }
