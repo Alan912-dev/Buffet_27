02bc56580ce378089406b6fcd61edaf1c97e5e4f
Alan912-dev <alanlushchichenko@gmail.com>
2026-09-09 21:44:02 +0000

    update

--- Java/000_Labs/003_output/base_code/starter.java
+++ Java/000_Labs/003_output/base_code/starter.java
@@ -9,12 +9,9 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		String Alan = ("Happy Birthday to you.");
-		String Lushchichenko = ("Happy Birthday dear");
-		String Nina = ("Lev.");
-		System.out.println(Alan);
-		System.out.println(Alan);
-		System.out.println(Lushchichenko + Nina);
-		System.out.println(Alan);
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, to you.");
+		System.out.println("Happy Birthday, dear Bob.");
+		System.out.println("Happy Birthday, to you.");
 	}
-}
+} 
--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -9,6 +9,12 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		
+		int Alan = 5*7/2%3-1;
+        int Nina = 10%3-4*7+2;
+        int Lev = (3-7/2*5)%10;
+		System.out.println("1) 5*7/2%3-1=" + Alan);
+		System.out.println("2) 10%3-4*7+2=" + Nina);
+		System.out.println("3) (3-7/2*5)%10=" + Lev);
 	}
 }
