ea0822bfe820d006cf0b3db697822cdb75369f94
Alan912-dev <alanlushchichenko@gmail.com>
2026-09-18 21:44:18 +0000

    update

--- Java/102_Exercises/09_Next5/base_code/starter.java
+++ Java/102_Exercises/09_Next5/base_code/starter.java
@@ -7,6 +7,32 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+		int alan = 5;
+		int lev = 6;
+		int nina = 7;
+		int mom = 8;
+		int n = 9;
+		int k = 10;
+		int l = 15;
+		int m = 20;
+		int o = 25;
+		int p = 30;
+		int q = 100;
+		double r = 0.05;
+		double s = 0.5;
+		System.out.println("Please enter a number: " + alan);
+		System.out.println("Here are the next " + alan + " numbers!");
+		System.out.println(alan + ", " + lev + ", " + nina + ", " + mom + ", " + n + ", " + k);
+		System.out.println("Here are the next " + alan + " multiples of " + alan + "! ");
+		System.out.println(alan + ", " + k + ", " + l + ", " + m +", " + o + ", " + p);
+		System.out.println("Here is " + alan + " divided by " + q + "!");
+		System.out.println(r);
+		System.out.println("Here is " + alan + " divided by " + k + "!");
+		System.out.println(s);
+
+	
+	
+
 
 
 	}
